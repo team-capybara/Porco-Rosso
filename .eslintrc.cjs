f0e062ec6acf6027ca59797bcee65261e2ae449a
dudupeng3105 <donguk3105@naver.com>
2024-06-04 23:45:11 +0900

    [#2] chore : 초기 개발 환경 설정(린트 및 깃 훅 설정)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,50 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'node_modules',
+    'build',
+    'coverage',
+    'public',
+    'dist',
+    '*.config.js',
+    '.eslintrc.cjs',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-refresh',
+    'react-hooks',
+    '@typescript-eslint',
+    'jsx-a11y',
+    'prettier',
+    '@tanstack/query',
+  ],
   rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};

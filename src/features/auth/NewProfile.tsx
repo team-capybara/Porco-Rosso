ed6436e1b4b40f5430f48c9e936a2f05838cf427
Hyeon <window8397@gmail.com>
2024-06-09 19:57:31 +0900

    [#3] 회원가입 Step2, 3 임시 마크업

--- src/features/auth/NewProfile.tsx
+++ src/features/auth/NewProfile.tsx
@@ -3,8 +3,10 @@ import { NewProfileProps } from './types/index';
 import classnames from 'classnames/bind';
 import styles from './newProfile.module.scss';
 import BackNavigation from './components/BackNavigation';
-import StepOne from './components/StepOne';
+// import StepOne from './components/StepOne';
+import StepTwo from './components/StepTwo';
 import NextStepLink from './components/NextStepLink';
+// import StepThree from './components/StepThree';
 
 const cn = classnames.bind(styles);
 
@@ -13,8 +15,12 @@ const NewProfile = (props: NewProfileProps) => {
   return (
     <div className={cn('new_profile')}>
       <div>
+        {/* todo: StepThree에서 BackNavigation 미노출 부탁드립니다. */}
         <BackNavigation />
-        <StepOne />
+        {/* todo: StepOne, StepTwo, StepThree 상황에 맞게 노출부탁드립니다. */}
+        {/* <StepOne /> */}
+        <StepTwo />
+        {/* <StepThree /> */}
       </div>
       <div className={cn('wrap_next_step_link')}>
         <NextStepLink />
---
+++ src/features/auth/components/StepThree.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './stepThree.module.scss';
+
+const cn = classnames.bind(styles);
+
+const StepThree = () => {
+  return (
+    <div className={cn('step_three')}>
+      <h2 className={cn('title')}>
+        축하드려요!
+        <br />
+        이제 00님의 소중한 모임,
+        <br />
+        모이미에서 차곡차곡 쌓아가요
+      </h2>
+    </div>
+  );
+};
+
+export default StepThree;

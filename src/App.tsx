d15f21373964de07ca96dd8e3300593189471dbb
Hyeon <window8397@gmail.com>
2024-06-09 17:48:07 +0900

    [#3] 로그인 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './styles/css/common.css';
+import './styles/scss/common.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
 

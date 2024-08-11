4993e55c4b1440cf1f21b2041cedccb74603b37f
dudupeng3105 <dudupeng3105@naver.com>
2024-08-11 23:08:48 +0900

    [#24] feat : 이미지 프로세싱 작업 추가

--- src/api/config.ts
+++ src/api/config.ts
@@ -6,7 +6,7 @@ export const mockAxios = axios.create({
   headers: {
     accept: '/',
     Authorization:
-      'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiaXNzIjoiY2FweWJhcmEiLCJyb2xlcyI6IkFETUlOIiwiaWF0IjoxNzE1MDAyNDYzLCJleHAiOjE3MjI3Nzg0NjN9.eeDxdX6__dE0raAMdKbXt0fwsMfDTAww0TfJmflkEPyHZQar5DDi1WSPZbgQx9T7l25F4gFWsvTPz8IqumV-Kg',
+      'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaXNzIjoiY2FweWJhcmEiLCJyb2xlcyI6Ik1FTUJFUiIsImlhdCI6MTcyMjg2OTg5M30.V3xnnSSEz5ykPrZB5cFCTZ5oLApY30EJ7Mgv0UYWOxNJbjgCngoD9HiQ5SP4FFHtd327_1OcmQdOv-mQ3LcgUg',
   },
 });
 
--- src/features/auth/components/signup/ProfileImage.tsx
+++ src/features/auth/components/signup/ProfileImage.tsx
@@ -3,18 +3,53 @@ import classnames from 'classnames/bind';
 import styles from './profileImage.module.scss';
 import IconEdit16X16 from '../../../../assets/svg/icon/IconEdit16X16';
 import { ProfileImageProps } from '../../types';
+import imageCompression from 'browser-image-compression';
+import heic2any from 'heic2any';
 
 const cn = classnames.bind(styles);
 
 const ProfileImage = ({ value, onChange, setProfile }: ProfileImageProps) => {
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //heif convert 배포해서 테스트
+  const heifConvert = async (file: File) => {
+    try {
+      const convertedBlob = await heic2any({
+        blob: file,
+        toType: 'image/jpeg',
+      });
+      console.log(convertedBlob, 'convertedBLob');
+      const blobItem = Array.isArray(convertedBlob)
+        ? convertedBlob[0]
+        : convertedBlob;
+      console.log(blobItem, 'blobItem');
+      const convertedFile = new File(
+        [blobItem],
+        file.name.split('.')[0] + '.jpg', // 파일 이름 설정
+        { type: 'image/jpeg', lastModified: new Date().getTime() } // 파일 속성 설정
+      );
+      console.log(convertedFile, 'convertedFile');
+      return convertedFile;
+    } catch (error) {
+      console.error('Error converting HEIF to JPEG:', error);
+      return file;
+    }
+  };
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
-      const validExtensions = ['image/jpeg', 'image/png'];
+      const validExtensions = [
+        'image/jpeg',
+        'image/png',
+        'image/heic',
+        'image/heif',
+      ];
       const maxSizeInBytes = 5 * 1024 * 1024; // 5MB
+      console.log(file, 'file에서 확장자가 뭐로 뜨지');
 
       if (!validExtensions.includes(file.type)) {
-        alert('허용된 확장자는 JPEG, PNG입니다.');
+        alert('허용된 확장자는 JPEG, PNG, HEIF입니다.');
         return;
       }
 
@@ -31,13 +66,44 @@ const ProfileImage = ({ value, onChange, setProfile }: ProfileImageProps) => {
         }
       };
       reader.readAsDataURL(file);
-      onChange(event.target.files[0]);
+
+      // 이미지 프로세싱
+      // 1. HEIF 변환, 2. 이미지 압축
+      // HEIF 변환
+      let processedFile = file;
+      if (file.type === 'image/heif' || file.type === 'image/heic') {
+        processedFile = await heifConvert(file);
+        if (
+          processedFile.type === 'image/heif' ||
+          processedFile.type === 'image/heif'
+        ) {
+          alert('HEIF/HEIF 확장자 변환 에러');
+          return;
+        }
+      }
+
+      // 이미지 압축
+      const options = {
+        maxSizeMB: 0.2, // 사진 용량 max 200kb 제한
+        maxWidthOrHeight: 450, // resolution 450 * 450 제한
+        useWebWorker: true,
+      };
+
+      const compressedBlob = await imageCompression(processedFile, options);
+
+      // Blob을 File로 변환
+      const blobToFile = new File([compressedBlob], processedFile.name, {
+        type: compressedBlob.type,
+      });
+
+      onChange(blobToFile);
     }
   };
   return (
     <div className={cn('profile_image')}>
       <div className={cn('inner')}>
         {/* 이미지 태그에 프로필 사진이 들어가야 합니다. 마크업 부탁드립니다 */}
+        {/* 이미지 정사각형 제한은 없어서, 정사각형이 아닌 이미지도 들어갈 수 있습니다 */}
         <img src={value} alt="" style={{ width: '20px', height: '20px' }} />
         <label htmlFor="profile_file" className={cn('label')}>
           <span className={cn('icon_area')}>

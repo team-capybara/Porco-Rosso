4dca26bfc9405b4d17e2354799197ea84152f212
seunghyeon2 <seunghyeon2@nts-corp.com>
2024-10-22 00:09:43 +0900

    [#9] 알림 마크업

---
+++ src/assets/svg/icon/IconFlash24X24.tsx
@@ -0,0 +1,25 @@
+import { SVGProps } from 'react';
+
+const IconFlash24X24 = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      fill="none"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="1.5"
+        d="M6.091 13.28h3.09v7.2c0 1.68.91 2.02 2.02.76l7.57-8.6c.93-1.05.54-1.92-.87-1.92h-3.09v-7.2c0-1.68-.91-2.02-2.02-.76l-7.57 8.6c-.92 1.06-.53 1.92.87 1.92"
+      />
+    </svg>
+  );
+};
+
+export default IconFlash24X24;

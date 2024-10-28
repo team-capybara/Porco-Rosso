0891d2c7f81c09fdedf1e6cd0383fa8f80e947d4
Nuriring <Zsda3131@naver.com>
2024-10-28 23:44:34 +0900

    fix : 브릿지 호출 기능 수정

--- src/features/auth/OauthRedirectHandler.tsx
+++ src/features/auth/OauthRedirectHandler.tsx
@@ -17,7 +17,10 @@ const OauthRedirectHandler = () => {
       navigate('/', { state: { from: location } });
     } else {
       // 앱 메인으로 브릿징, 원래라면 활성화 해야하는 코드
-      goMain(accessToken);
+      // 앱 초기 오픈 시 브릿지가 등록되기 전 브릿지 호출 방지
+      setTimeout(() => {
+        goMain(accessToken);
+      }, 500);
       // 올드비로도 signup 페이지 테스트 필요할 때 활성화, 원래라면 비활성화
       // navigate('/signup', { state: { from: location } });
     }
